f31e9d35a5201466c965d539cca3b001768e0b75
Crowdbotics <team@crowdbotics.com>
2024-03-14 19:55:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-shape-47064",
+  url: "https://restless-shape-47064.botics.co",
+  api: "https://restless-shape-47064.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
